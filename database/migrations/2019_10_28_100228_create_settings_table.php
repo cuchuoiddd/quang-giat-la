97d946cd15b6f1e72c5f3fb103ffe58a002441e4
Tien Nguyen <cuchuoiddd@gmail.com>
2019-10-28 17:33:14 +0700

    qua qua

---
+++ database/migrations/2019_10_28_100228_create_settings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('logo')->nullable();
+            $table->string('banner')->nullable();
+            $table->string('text1_header')->nullable();
+            $table->string('text2_header')->nullable();
+            $table->string('text3_header')->nullable();
+            $table->string('phone')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('settings');
+    }
+}
---
+++ database/migrations/2019_10_28_100951_create_services_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateServicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title')->nullable();
+            $table->text('description')->nullable();
+            $table->integer('category_id')->nullable();
+            $table->string('images')->nullable();
+            $table->string('discount')->nullable();
+            $table->string('metatitle')->nullable();
+            $table->text('metadescription')->nullable();
+            $table->boolean('publish')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('services');
+    }
+}
