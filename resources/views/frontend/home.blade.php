7d0c4e04fe4404292e44754e0e701710286d1fa1
quangvv <quangphuong9685@gmail>
2019-11-03 01:20:05 +0700

    fix chat v3

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -195,18 +195,14 @@
             <!-- Load Facebook SDK for JavaScript -->
 
             </div>
-{{--                <div class="tawk-to-gv88">--}}
-{{--                    <div id="widget-btn-tawk_to" class="widget-btn-float-container widget-tooltip">--}}
-{{--                        <a href="javascript:void(0);" class="widget-menu-tawk-to" data-action="widget_current_channel = 'tawk_to'; penTawkToPopup();">--}}
-{{--                            <span class="button_tap"></span>--}}
-{{--                        </a>--}}
-{{--                        <div class="widget-menu-tawk-to"></div>--}}
-{{--                        {!!isset($setting->tawk_to_widget_code) ? $setting->tawk_to_widget_code : '' !!}--}}
-{{--                        <!--End of Tawk.to Script-->--}}
-{{--                        <div class="widget-tooltiptext" style="cursor: pointer;  background-color: rgba(29, 36, 62, 0.7); ">Chat trực tiếp cho chúng tôi--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
+                <div class="tawk-to-gv88">
+                    <div id="widget-btn-tawk_to" class="widget-btn-float-container widget-tooltip">
+                        <a href="javascript:void(0);" class="widget-menu-tawk-to"
+                           data-action="widget_current_channel = 'tawk_to'; penTawkToPopup();">
+                            <span class="button_tap"></span>
+                        </a>
+
+                    </div>
             </div>
         </div>
 	</footer>
