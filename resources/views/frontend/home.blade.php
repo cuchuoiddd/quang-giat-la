b0ee05410aea05a8cfce643fcc87b72840042f40
quangvv-dev <quangphuong9685@gmail.com>
2019-11-05 10:30:31 +0700

    change messager

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -45,27 +45,6 @@
 </head>
 
 <body>
-<script>
-    window.fbAsyncInit = function () {
-        FB.init({
-            appId: '799857890468063',
-            xfbml: true,
-            version: 'v5.0'
-        });
-        FB.AppEvents.logPageView();
-    };
-
-    (function (d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) {
-            return;
-        }
-        js = d.createElement(s);
-        js.id = id;
-        js.src = "https://connect.facebook.net/en_US/sdk.js";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-</script>
 <header class="header-top mb50">
     <div class="container">
         <nav class="navbar navbar-expand-lg navbar-light">
